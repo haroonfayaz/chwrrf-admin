1e41754d0ee7dda0b6df73e0455fa387b872fa34
haroonfayaz <harounfayaz@gmail.com>
2024-01-08 21:29:20 +0530

    created notifications calls

--- src/api.js
+++ src/api.js
@@ -1,40 +1,68 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://13.40.5.17:8080/api/';
+const baseURL = "http://13.40.5.17:8080/api/";
 const instance = axios.create({
   baseURL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export const getAllEvents = async () => {
   try {
-    const response = await instance.get('getAllEvents');
+    const response = await instance.get("getAllEvents");
     return response.data;
   } catch (error) {
-    console.error('Error fetching events:', error);
+    console.error("Error fetching events:", error);
     throw error;
   }
 };
 
 export const createEvent = async (eventData) => {
   try {
-    const response = await instance.post('createEvent', eventData);
+    const response = await instance.post("createEvent", eventData);
     return response.data;
   } catch (error) {
-    console.error('Error creating event:', error);
+    console.error("Error creating event:", error);
     throw error;
   }
 };
 
 export const updateEvent = async (eventId, eventData) => {
-    try {
-      const response = await instance.put(`updateEvent/${eventId}`, eventData);
-      return response.data;
-    } catch (error) {
-      console.error('Error updating event:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await instance.put(`updateEvent/${eventId}`, eventData);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating event:", error);
+    throw error;
+  }
+};
+
+export const getAllNotifications = async () => {
+  try {
+    const response = await instance.get("getAllNotify");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching notification:", error);
+    throw error;
+  }
+};
+export const createNotify = async (formData) => {
+  try {
+    const response = await instance.post("createNotify", formData);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating notification:", error);
+    throw error;
+  }
+};
+export const updateNotify = async (notifyId, formData) => {
+  try {
+    const response = await instance.put(`updateNotify/${notifyId}`, formData);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating notification:", error);
+    throw error;
+  }
+};
