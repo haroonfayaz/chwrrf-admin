edfb93b0baec988ac5c57619ab933092cdb00853
haroonfayaz <harounfayaz@gmail.com>
2024-02-12 19:35:28 +0530

    fixed homepage issue and added loader file for now

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
-import Layout from './components/Layout';
-import Events from './pages/Events';
-import Notifications from './pages/Notifications';
-import FuturePlans from './pages/FuturePlans';
-
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+import Layout from "./components/Layout";
+import Events from "./pages/Events";
+import Notifications from "./pages/Notifications";
+import FuturePlans from "./pages/FuturePlans";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#fefefe'
+      main: "#fefefe",
     },
-    secondary: purple
+    secondary: purple,
   },
   typography: {
-    fontFamily: 'Quicksand',
+    fontFamily: "Quicksand",
     fontWeightLight: 400,
     fontWeightRegular: 500,
     fontWeightMedium: 600,
     fontWeightBold: 700,
-  }
+  },
 });
 
 function App() {
- 
-
   return (
     <>
-        <ThemeProvider theme={theme}>
-            <Router>
-              <Layout>
-                      <Routes>
-                        <Route path="/" element={<Events/>} />
-                        <Route path="/notification" element={<Notifications/>} />
-                        <Route path="/plans" element={<FuturePlans/>} />
-
-                      </Routes>
-              </Layout>
-            </Router>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Navigate to="/events" />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/notification" element={<Notifications />} />
+              <Route path="/plans" element={<FuturePlans />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
